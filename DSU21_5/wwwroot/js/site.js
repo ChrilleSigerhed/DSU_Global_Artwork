c4a897f2d4d6af6355ce7c7a452066134f55f182
jesper magnusson <Jeppe.M@live.se>
2021-02-04 13:38:36 +0100

    Added JS-code to site.js for chat.

--- DSU21_5/wwwroot/js/site.js
+++ DSU21_5/wwwroot/js/site.js
@@ -26,3 +26,39 @@ jQueryAjaxDelete = form => {
     return false;
 }
 
+const connection = new signalR.HubConnectionBuilder().withUrl("/chatHub").build();
+
+async function start() {
+    try {
+        await connection.start();
+        console.log("SignalR Connected");
+    } catch (err) {
+        console.log(err);
+        setTimeout(start, 5000);
+    }
+};
+
+connection.onclose(start);
+
+start();
+
+
+
+
+document.getElementById("sendMessageBtn").addEventListener('click', async function () {
+    const message = document.getElementById("message").value;
+    const name = document.getElementById("name").value;
+    try {
+        await connection.invoke("SendMessage", name, message);
+    } catch (err) {
+        console.log(err);
+    }
+})
+
+connection.on("ReceiveMessage", (name, message) => {
+    const rec_msg = name + ": " + message;
+    const li = document.createElement("li");
+    li.textContent = rec_msg;
+    document.getElementById("messageList").appendChild(li);
+})
+
