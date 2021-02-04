0d3d292f6bed800136c4958c21e55795e7c74517
Ludvig Blixö <ludvigblixo@hotmail.com>
2021-02-04 18:09:43 +0100

    Added art to testuser and fixed the serchbar in community

--- DSU21_5/Controllers/ShowroomController.cs
+++ DSU21_5/Controllers/ShowroomController.cs
@@ -22,7 +22,7 @@ namespace DSU21_5.Controllers
         [Route("Showroom")]
         public async Task<IActionResult> Index(string Id)
         {
-            Id = "638aa03d-c00c-4c9f-8e3e-2206b57f404d";
+            Id = "aca28772-e443-4b6f-a7bb-6088c20131e6";
             var postedArt = await artRepository.GetPostedArtFromUniqueUser(Id);
             var member = await memberRepository.GetMember(Id);
             
