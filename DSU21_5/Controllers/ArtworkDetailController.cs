bca7cfa9042ab4f700a14d38a0d4500e180701cf
ChrilleSigerhed <57347249+ChrilleSigerhed@users.noreply.github.com>
2021-02-08 12:16:04 +0100

    Added navigation to DetailPage with correct info gathered from DetailsViewModel

--- DSU21_5/Controllers/ArtworkDetailController.cs
+++ DSU21_5/Controllers/ArtworkDetailController.cs
@@ -2,15 +2,30 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using DSU21_5.Data;
+using DSU21_5.Models.ViewModel;
 using Microsoft.AspNetCore.Mvc;
 
 namespace DSU21_5.Controllers
 {
     public class ArtworkDetailController : Controller
     {
-        public IActionResult Index()
+        public IArtRepository ArtRepository { get; set; }
+        public IMemberRepository MemberRepository { get; set; }
+        public ArtworkDetailViewModel artworkDetailViewModel { get; set; }
+        public ArtworkDetailController(IArtRepository artRepository, IMemberRepository memberRepository)
         {
-            return View();
+            ArtRepository = artRepository;
+            MemberRepository = memberRepository;
+        }
+        public async Task<IActionResult> Index(int Id)
+        {
+            var artwork = ArtRepository.GetArtworkForUser(Id);
+            var allArtworksFromUser = await ArtRepository.GetPostedArtFromUniqueUser(artwork.UserId);
+            var user = await MemberRepository.GetMember(artwork.UserId);
+
+            artworkDetailViewModel = new ArtworkDetailViewModel(allArtworksFromUser,artwork, user);
+            return View(artworkDetailViewModel);
         }
     }
 }
