37d3fa2671b39d18bda2b96646b31677848636dd
Lyn Santiago Enriquez <lysa1900@student.miun.se>
2021-02-11 10:36:51 +0100

    Friends function moved to profile

--- DSU21_5/Controllers/CommunityController.cs
+++ DSU21_5/Controllers/CommunityController.cs
@@ -17,70 +17,39 @@ namespace DSU21_5.Controllers
         public IMemberRepository MemberRepository {get; set;}
         public IImageRepository ImageRepository { get; set; }
         public IArtRepository ArtRepository { get; set; }
-        public IRelationshipRepository RelationshipRepository { get; set; }
         public ProfileViewModel ProfileViewModel { get; set; }
-
         public CommunityViewModel CommunityViewModel { get; set; }
-        private readonly UserManager<ApplicationUser> _userManager;
 
 
-        public CommunityController(IMemberRepository memberRepository, IImageRepository imageRepository, IArtRepository artRepository, IRelationshipRepository relationshipRepository, UserManager<ApplicationUser> userManager)
+        public CommunityController(IMemberRepository memberRepository, IImageRepository imageRepository, IArtRepository artRepository, IRelationshipRepository relationshipRepository)
         {
             MemberRepository = memberRepository;
             ImageRepository = imageRepository;
             ArtRepository = artRepository;
-            RelationshipRepository = relationshipRepository;
-            _userManager = userManager;
-        }
-
-        public string GetCurrentUserId()
-        {
-            var currentUserId = _userManager.GetUserId(HttpContext.User);
-            return currentUserId;
+           
         }
 
         public async Task<IActionResult> Index()
         {
-            var userId = GetCurrentUserId();
-            var pendingFriendsRelationships = await RelationshipRepository.GetPendingRelationship(userId);
-            var acceptedFriendsRelationships = await RelationshipRepository.GetRelationshipsByUserId(userId);
-            List<Member> members = await MemberRepository.GetAllMembers();
-            //List<Member> listOfMembers = await MemberRepository.GetAllMembers();
-            List<Image> listOfImages = ImageRepository.GetAllImagesFromDbConnectedToUsers(members);
+            //List<Member> members = await MemberRepository.GetAllMembers();
+            List<Member> listOfMembers = await MemberRepository.GetAllMembers();
+            List<Image> listOfImages = ImageRepository.GetAllImagesFromDbConnectedToUsers(listOfMembers);
 
-            var pendingFriends = new List<Member>();
 
-            foreach (var pending in pendingFriendsRelationships)
-            {
-                var member = await MemberRepository.GetMember(pending.Requester);
-                pendingFriends.Add(member);
-            }
 
-            var acceptedFriends = new List<Member>();
 
-            foreach (var friend in acceptedFriendsRelationships)
-            {
-                Member member;
-                if (friend.Requester == userId)
-                    member = await MemberRepository.GetMember(friend.Requestee);
-                else
-                    member = await MemberRepository.GetMember(friend.Requester);
+            //var viewModel = new CommunityViewModel(listOfImages, members)
+            //{
+            //    Members = members
+            //};
 
-                acceptedFriends.Add(member);
-            }
-            var viewModel = new CommunityViewModel(listOfImages, members)
-            {
-                AcceptedFriends = acceptedFriends,
-                PendingFriends = pendingFriends,
-                Members = members
-            };
+            //return View(viewModel);
 
-            return View(viewModel);
 
-            
 
-            //CommunityViewModel = new CommunityViewModel(listOfImages, listOfMembers);
-            //return View(CommunityViewModel);
+            CommunityViewModel = new CommunityViewModel(listOfImages, listOfMembers);
+            return View(CommunityViewModel);
+        
         }
     
         public async Task<IActionResult> Profile(string Id)
@@ -96,38 +65,6 @@ namespace DSU21_5.Controllers
         
         }
 
-        public async Task<IActionResult> SendFriendRequest(string id)
-        {
-            Relationship relationship = new Relationship()
-            {
-                Requester = GetCurrentUserId(),
-                Requestee = id
-            };
-
-            await RelationshipRepository.Create(relationship);
-
-            return RedirectToAction("Index");
-        }
-
-
-        public async Task<IActionResult> AcceptFriendRequest(string id)
-        {
-            string requestee = GetCurrentUserId();
-            string requester = id;
-            await RelationshipRepository.AcceptRelationshipRequest(requester, requestee);
-
-            return RedirectToAction("Index");
-        }
-
-
-        public async Task<IActionResult> DeclineFriendRequest(string id)
-        {
-            string requestee = GetCurrentUserId();
-            string requester = id;
-            await RelationshipRepository.DenyRelationshipRequest(requester, requestee);
-
-            return RedirectToAction("Index");
-
-        }
+       
     }
 }
