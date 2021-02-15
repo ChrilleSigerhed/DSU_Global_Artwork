1b14a7b6abb97e7bc1d08090bbe949770f6099cb
Lyn Santiago Enriquez <lysa1900@student.miun.se>
2021-02-15 13:30:26 +0100

    unittest

---
+++ DSU21_5Tests/ArtworkDetailViewModelTest.cs
@@ -0,0 +1,34 @@
+﻿using DSU21_5.Models;
+using DSU21_5.Models.ViewModel;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace DSU21_5Tests
+{
+    public class ArtworkDetailViewModelTest
+    {
+        //[Fact]
+        //public void CheckIfViewModelReturnsCorrectArtName_ReturnsCorrectValue()
+        //{
+        //    List<Artwork> artworks = new List<Artwork>();
+        //    Member member = new Member();
+        //    Artwork art = new Artwork()
+        //    {
+        //        ArtName = "Mona Lisa"
+        //    };
+
+        //    ArtworkDetailViewModel artworkViewModel = new ArtworkDetailViewModel(artworks, art, member);
+
+        //    var expected = art.ArtName;
+        //    var actual = artworkViewModel.ListOfArt[0].ArtName;
+
+        //    Assert.Equal(expected, actual);
+
+        //}
+
+
+
+    }
+}
