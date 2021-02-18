cd40bb2001c7481ed4f917d0f632be83f1aa05e4
Malin Jönsson <malin9327@outlook.com>
2021-02-18 10:09:27 +0100

    added interfaces to folders

--- DSU21_5/Models/Interfaces/IArtwork.cs
+++ DSU21_5/Models/Interfaces/IArtwork.cs
@@ -14,22 +14,17 @@ namespace DSU21_5.Models
 
         [Key]
         public int ArtworkId { get; set; }
-        //[Required]
+        [Required]
         [Column(TypeName = "nvarchar(100)")]
         public string ImageName { get; set; }
-        //[Required]
+        [Required]
         [Column(TypeName = "nvarchar(100)")]
         public string ArtName { get; set; }
-        //[Required]
+        [Required]
         [Column(TypeName = "nvarchar(100)")]
         public string UserId { get; set; }
-        ////[Required]
-        //[Column(TypeName = "nvarchar(100)")]
-        //public string Firstname { get; set; }
-        ////[Required]
-        //[Column(TypeName = "nvarchar(100)")]
-        //public string Lastname { get; set; }
-        //[Column(TypeName = "nvarchar(100)")]
+
+        [Column(TypeName = "nvarchar(100)")]
         public string Description { get; set; }
         [NotMapped]
         [DisplayName("Upload File")]
